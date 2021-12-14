e893d9a144181aaf5020fa8815ac90ac26447e95
padrisimo <chamizho@gmail.com>
2021-12-14 14:44:27 +0100

    feat: first shot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+  loaderUrl: "build/capsulecorp.loader.js",
+  dataUrl: "build/capsulecorp.data.unityweb",
+  frameworkUrl: "build/capsulecorp.framework.js.unityweb",
+  codeUrl: "build/capsulecorp.wasm.unityweb",
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(function () {
+    unityContext.on("Jump", function (crounch) {
+      console.log('saltando', crounch )
+    });
+  }, []);
+  return <Unity style={{maxWidth:'100vw'}} unityContext={unityContext} />;
 }
-
 export default App;
