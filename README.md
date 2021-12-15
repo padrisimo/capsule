c1fdedac5e5b86103eb7b815b7d10dc1ae5c0c1f
padrisimo <chamizho@gmail.com>
2021-12-15 10:35:52 +0100

    fear: sand mode to send

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # React Unity Playground
 
-https://media.giphy.com/media/PDsgxQoXvUZGg/giphy.gif
-
 ![image](https://media.giphy.com/media/PDsgxQoXvUZGg/giphy.gif)
--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React, { useEffect } from "react";
-import Unity, { UnityContext } from "react-unity-webgl";
+import React, { useEffect, useState } from 'react';
+import Unity, { UnityContext } from 'react-unity-webgl';
 
 const unityContext = new UnityContext({
-  loaderUrl: "build/capsulecorp.loader.js",
-  dataUrl: "build/capsulecorp.data.unityweb",
-  frameworkUrl: "build/capsulecorp.framework.js.unityweb",
-  codeUrl: "build/capsulecorp.wasm.unityweb",
+  loaderUrl: 'build/capsulecorp.loader.js',
+  dataUrl: 'build/capsulecorp.data.unityweb',
+  frameworkUrl: 'build/capsulecorp.framework.js.unityweb',
+  codeUrl: 'build/capsulecorp.wasm.unityweb',
 });
 
 function App() {
-  useEffect(function () {
-    unityContext.on("Jump", function (crounch) {
-      console.log('saltando', crounch )
-    });
-  }, []);
-  return <Unity style={{maxWidth:'100vw'}} unityContext={unityContext} />;
+  const [jumpCount, setJumpCount] = useState(0);
+  const [sandMode, setSandMode] = useState(false);
+  useEffect(
+    function () {
+      unityContext.on('Jump', function (crounch) {
+        console.log('saltando', crounch);
+        setJumpCount(jumpCount + 1);
+      });
+    },
+    [jumpCount]
+  );
+
+  const toggleSandMode = () => {
+    setSandMode(!sandMode);
+  };
+
+  return (
+    <>
+      <div className="topbar">
+        <span>jumps:{jumpCount}</span>
+        <span>
+          sand mode: <input type="checkbox" onClick={() => toggleSandMode()} />
+        </span>
+      </div>
+      <Unity style={{ maxWidth: '100vw' }} unityContext={unityContext} />
+    </>
+  );
 }
 export default App;
